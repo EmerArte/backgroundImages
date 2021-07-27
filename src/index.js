672b63fa6aacf81c4e41519cde27807962671071
Emer Arteaga <arteagaemer@gmail.com>
2021-07-27 09:09:09 -0500

    Carga de imagenes desde unsplash.com

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const link =
+  "https://api.unsplash.com/photos/?client_id=JaG19GvYsAJ3tA30CeTCDzK6b94avjuZ_wvYLF6Of0M";
+const boton = document.querySelector("#add");
+const container = document.querySelector("#img");
+let contador=0;
 
-console.log('Happy hacking :)')
+fetch(link)
+  .then((response) => response.json())
+  .then((data) => {
+    //data.forEach((element) => {
+      //let img = document.createElement("img");
+      //const cont = document.createElement("div");
+      //img.classList = "w-full";
+      //img.src = element.urls.full;
+      //cont.classList = "w-1/2 mx-auto box-border mt-4";
+      
+      boton.addEventListener("click", (event) => {
+        let img = document.createElement("img");
+        const cont = document.createElement("div");
+        img.classList = "w-full";
+        img.src=data[contador].urls.full;
+        cont.classList = "w-1/2 mx-auto box-border mt-4";
+        cont.append(img);
+        container.append(cont);
+        contador++;
+    });
+    //});
+  });
+
